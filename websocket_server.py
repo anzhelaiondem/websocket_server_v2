2c5a17db6f40c14f87755a073487dff262f43579
anzhelaiondem <anzhlela.iondem.fd@gmail.com>
2021-06-03 12:09:03 +0400

    Solve the issue with RA and DEC calculation.
    Remove the double calculation of RA and DEC.
    Refine try-except blocks.
    Add missing docstring comments.

--- websocket_server.py
+++ websocket_server.py
@@ -9,13 +9,35 @@ async def sleep(websocket):
     """This function implements 10 seconds sleeping effect (the SLEEP_TIME is fixed in the config.py)
      and every second checks the connectivity of the client (websocket)."""
     t = 1
-    try:
-        while t <= config.SLEEP_TIME:
-            await websocket.send("")
-            await asyncio.sleep(1)
-            t += 1
-    except (websockets.ConnectionClosedError, websockets.ConnectionClosedOK) as err:
-        return err
+    while t <= config.SLEEP_TIME:
+        await websocket.send("")
+        await asyncio.sleep(1)
+        t += 1
+
+
+async def calculate_and_send_moon_ra_dec(websocket):
+    """The function calculates the difference of time between the fixed point and the current time,
+    calculates the RA and the DEC of the Moon (based on the difference), sends it to the client,
+    sleeps for mentioned amount of time and again calculates the difference (from the previous moment)
+     and sends new RA and DEC it to client """
+    new_ra = config.FIX_RA_IN_SEC
+    new_dec = config.FIX_DEC_IN_SEC
+    start_date = config.START_DATE
+    while True:
+        time_in_seconds = int((config.dt.datetime.now() - start_date).total_seconds())
+        start_date = config.dt.datetime.now()
+        for sec in range(time_in_seconds):
+            new_ra = new_ra + config.AV_CHANGE_OF_M_RA_S
+            if new_ra > 86399:
+                new_ra = new_ra % 86399
+            if 18 * 3600 < new_ra < 24 * 3600 or 0 < new_ra < 6 * 3600:
+                new_dec = new_dec + config.AV_CHANGE_OF_M_DEC_S
+            elif 6 * 3600 <= new_ra <= 18 * 3600:
+                new_dec = new_dec - config.AV_CHANGE_OF_M_DEC_S
+        ra = f'{(int(new_ra // 3600))}:{int((new_ra % 3600) // 60)}:{int((new_ra % 3600) % 60)}'
+        dec = f'{int(new_dec // 3600)}:{int((new_dec % 3600) // 60)}:{int((new_dec % 3600) % 60)}'
+        await websocket.send(f"RA:{ra} DEC:{dec}")
+        await sleep(websocket)
 
 
 async def producer_handler(websocket, path) -> None:
@@ -27,9 +49,7 @@ async def producer_handler(websocket, path) -> None:
     config.logger.info(f"Client {websocket.remote_address[:2]} is CONNECTED. Active clients N: {len(config.USERS)}")
     try:
         while True:
-            await calculate_and_send_moon_ra_dec(config.FIX_RA, config.FIX_DEC, websocket)
-            #await websocket.send(msg)
-            #await sleep(websocket)
+            await calculate_and_send_moon_ra_dec(websocket)
     except websockets.ConnectionClosedError as err:
         config.USERS.remove(websocket)
         config.logger.info(f"Client {websocket.remote_address[:2]} is DISCONNECTED. "
@@ -42,30 +62,8 @@ async def producer_handler(websocket, path) -> None:
         config.logger.error(f"Client {websocket.remote_address[:2]} has closed the connection with CloseOK: {err}")
 
 
-async def calculate_and_send_moon_ra_dec(FIX_RA, FIX_DEC, websocket):
-    new_ra = config.FIX_RA_IN_SEC
-    new_dec = config.FIX_DEC_IN_SEC
-    while True:
-        time_in_seconds = int((config.dt.datetime.now() - config.START_DATE).total_seconds())
-        try:
-            for sec in range(time_in_seconds):
-                new_ra = new_ra + config.AV_CHANGE_OF_M_RA_S
-                if new_ra > 86399:
-                    new_ra = new_ra % 86399
-                if 18 * 3600 < new_ra < 24 * 3600 or 0 < new_ra < 6 * 3600:
-                    new_dec = new_dec + config.AV_CHANGE_OF_M_DEC_S
-                elif 6 * 3600 <= new_ra <= 18 * 3600:
-                    new_dec = new_dec - config.AV_CHANGE_OF_M_DEC_S
-            ra = f'{(int(new_ra // 3600))}:{int((new_ra % 3600) // 60)}:{int((new_ra % 3600) % 60)}'
-            dec = f'{int(new_dec // 3600)}:{int((new_dec % 3600) // 60)}:{int((new_dec % 3600) % 60)}'
-            await websocket.send(f"{ra} {dec}")
-            await sleep(websocket)
-        except (websockets.ConnectionClosedError, websockets.ConnectionClosedOK) as err:
-            return err
-
-
 def create_and_run_websocket(ip: str, port: int):
-    """ The function takes as parameters ip and port and returns activated websocket server."""
+    """ The function takes as parameters ip and port and returns activated (listening) websocket server."""
     return websockets.serve(producer_handler, ip, port)
 
 
