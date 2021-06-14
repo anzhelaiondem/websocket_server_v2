e717fdcaaf3940b5bdcb16e49c9bbf8c935d0c91
anzhelaiondem <anzhlela.iondem.fd@gmail.com>
2021-06-14 19:15:16 +0400

    Correct the DEC calculation.

--- moon_ra_dec.py
+++ moon_ra_dec.py
@@ -9,7 +9,7 @@ FIX_RA = [22, 46, 54]
 FIX_DEC = [-13, 27, 21]
 # Dictionary of average change(value) of RA and DEC for the mentioned time slots/key.
 AV_DELTA_RA = {"0-5": 0.032, "5-12": 0.036, "12-17": 0.038, "17-0": 0.041}
-AV_DELTA_DEC = {"5-8 and 17-20": 0.065, "0-5, 8-17, and 20-0": 0.182}
+AV_DELTA_DEC = {"5-9 and 17-20": 0.055, "0-5, 9-17, and 20-0": 0.182}
 
 
 def convert_hours_minutes_seconds_to_seconds(time_list: List[int]):
@@ -38,25 +38,25 @@ def calculate_moon_ra_dec(fix_ra: List[int], fix_dec: List[int], start_date, av_
     for sec in range(1, time_in_seconds):
         if 0 <= new_ra < 5 * 3600:
             new_ra = new_ra + av_delta_ra.get("0-5")
-            new_dec = new_dec + av_delta_dec.get("0-5, 8-17, and 20-0")
+            new_dec = new_dec + av_delta_dec.get("0-5, 9-17, and 20-0")
         elif 5 * 3600 <= new_ra < 7 * 3600:
             new_ra = new_ra + av_delta_ra.get("5-12")
-            new_dec = new_dec + av_delta_dec.get("5-8 and 17-20")
-        elif 7 * 3600 <= new_ra < 8 * 3600:
+            new_dec = new_dec + av_delta_dec.get("5-9 and 17-20")
+        elif 7 * 3600 <= new_ra < 9 * 3600:
             new_ra = new_ra + av_delta_ra.get("5-12")
-            new_dec = new_dec - av_delta_dec.get("5-8 and 17-20")
-        elif 8 * 3600 <= new_ra < 12 * 3600:
+            new_dec = new_dec - av_delta_dec.get("5-9 and 17-20")
+        elif 9 * 3600 <= new_ra < 12 * 3600:
             new_ra = new_ra + av_delta_ra.get("5-12")
-            new_dec = new_dec - av_delta_dec.get("0-5, 8-17, and 20-0")
+            new_dec = new_dec - av_delta_dec.get("0-5, 9-17, and 20-0")
         elif 12 * 3600 <= new_ra < 17 * 3600:
             new_ra = new_ra + av_delta_ra.get("12-17")
-            new_dec = new_dec - av_delta_dec.get("5-8 and 17-20")
+            new_dec = new_dec - av_delta_dec.get("0-5, 9-17, and 20-0")
         elif 17 * 3600 <= new_ra < 19 * 3600:
             new_ra = new_ra + av_delta_ra.get("17-0")
-            new_dec = new_dec - av_delta_dec.get("0-5, 8-17, and 20-0")
+            new_dec = new_dec - av_delta_dec.get("5-9 and 17-20")
         elif 19 * 3600 <= new_ra < 24 * 3600:
             new_ra = new_ra + av_delta_ra.get("17-0")
-            new_dec = new_dec + av_delta_dec.get("0-5, 8-17, and 20-0")
+            new_dec = new_dec + av_delta_dec.get("0-5, 9-17, and 20-0")
         if new_ra > 86399:
             new_ra = new_ra % 86399
     ra_h, ra_m, ra_s = convert_seconds_to_hours_minutes_seconds(new_ra)
